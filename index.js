0cdac354cd7e921586453a9a3b3b92398714bc8b
Glenn Robey <glennrobey95@gmail.com>
2025-09-30 11:29:42 -0500

    creating the DOM layout now, after that I should be done

--- index.js
+++ index.js
@@ -67,3 +67,39 @@ function renderSelectedPlayer() {
 
   document.getElementById("remove-btn").addEventListener("click", removePlayer);
 }
+
+async function removePlayer() {
+  if (!state.selectedPlayer) return;
+
+  try {
+    await fetch(`${API}/players/${state.selectedPlayer.id}`, {
+      method: "DELETE",
+    });
+
+    state.selectedPlayer = null;
+    fetchPlayers();
+    renderSelectedPlayer();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+document
+  .getElementById("add-player-form")
+  .addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const name = e.target.name.value;
+    const breed = e.target.breed.value;
+    try {
+      await fetch(PLAYERS_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name, breed }),
+      });
+      e.target.reset();
+      fetchPlayers();
+    } catch (err) {
+      console.error(err);
+    }
+  });
